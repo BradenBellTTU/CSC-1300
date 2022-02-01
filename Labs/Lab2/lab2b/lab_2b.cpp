72957b5b362ec4cedeedf4e545fb13b1c469ff81
BradenBellTTU <91089671+BradenBellTTU@users.noreply.github.com>
2022-02-01 10:48:01 -0600

    Lab2

--- Labs/Lab2/lab2b/lab_2b.cpp
+++ Labs/Lab2/lab2b/lab_2b.cpp
@@ -1,36 +1,48 @@
+/*
+	Title:  	lab_2b.cpp
+	Author:		Braden Bell
+	Date:		2-1-2022	
+	Purpose:	This program helps a user at a convience store to buy 2 different items and calculate the cost
+*/
 #include <iostream>
 #include <iomanip>
 #include <string>
 using namespace std;
 
 int main() {
+    //Variables
     const double SALES_TAX = 0.0975;
     string item1Name, item2Name;
     int item1Quantity, item2Quantity;
     double item1Cost, item2Cost, totalCost;
 
-    cout << "Please input the name of the first item: ";
+    //Get information for first item
+    cout << "\n\nPlease input the name of the first item: ";
     getline(cin, item1Name);
     cout << "Please input the number of " << item1Name << " bought: ";
     cin >> item1Quantity;
-    cin.ignore();
     cout << "Please input the price of " << item1Name << ": $";
     cin >> item1Cost;
-    cin.ignore();
     cout << "\n";
 
+    //Get information for second item
     cout << "Please input the name of the second item: ";
+    cin.ignore();
     getline(cin, item2Name);
     cout << "Please input the number of " << item2Name << " bought: ";
     cin >> item2Quantity;
-    cin.ignore();
     cout << "Please input the price of " << item2Name << ": $";
     cin >> item2Cost;
 
+    //Calculate the total without sales tax
     totalCost = (item1Quantity * item1Cost) + (item2Quantity + item2Cost);
+
+    //Adds sales tax to total
     totalCost = (totalCost * SALES_TAX) + totalCost;
 
-    cout << setprecision(2) << fixed << showpoint << "Your total bill is: $" << totalCost << endl;
+    //Output total to user
+    cout << setprecision(2) << fixed << showpoint << "\nYour total bill is: $" << totalCost << endl;
+
     return 0;
     
 }
