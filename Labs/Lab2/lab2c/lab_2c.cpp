50ccf7d799281d3aa7b325f2c504225f06c59ec4
BradenBellTTU <babell45@tntech.edu>
2022-02-04 16:53:07 -0600

    lab2c

--- Labs/Lab2/lab2c/lab_2c.cpp
+++ Labs/Lab2/lab2c/lab_2c.cpp
@@ -1,31 +1,41 @@
+/*
+        Title:          lab_2b.cpp
+        Author:         Braden Bell
+        Date:           2-1-2022
+        Purpose:        This program helps a user at a convience store to buy>
+*/
+
 #include <iostream>
+#include <iomanip>
 #include <string>
 using namespace std;
 
 
-int main {
+int main() {
 	string studentName;
 	double score1, score2, score3, average;
 
 	//Get student name
-	cout << "Enter the student's name: ";
+	cout << "\n\nEnter the student's name: ";
 	getline(cin, studentName);
-	cin.ignore();
 	
 	//Get test grades
 	cout << "Enter " << studentName << "'s first test grade: ";
-	cin >> score1
+	cin >> score1;
 	
 	cout << "Enter " << studentName << "'s second test grade: ";
-	cin >> score2
+	cin >> score2;
 
 	cout << "Enter " << studentName << "'s third test grade: ";
-	cin >> score3
+	cin >> score3;
 	
+
 	//Calculate average
+	cout << setprecision(2) << fixed << showpoint;
 	average = (score1 + score2 + score3) / 3;
 	
 	//Print average to user
-	cout << studentName << " has a test average of " << average << endl;
+	cout << "\n" << studentName << " has a test average of " << average << endl;
 	
+	return 0;
 }
