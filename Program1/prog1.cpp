65af497f1e71442cf83602835464d255291bba2e
BradenBellTTU <babell45@tntech.edu>
2022-02-05 00:27:19 -0600

    start of prog3

---
+++ Program1/prog1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+int main() {
+
+	
+	cout << "\n\n-------------------------------" << endl;
+	cout << "      Personality Test         " << endl;
+	cout << "-------------------------------" << endl;
+	cout << "This is a 12 question personality test designed to gather data" << endl;
+	cout << "from 4 different aspects of your personality." << endl.
+
+	cout << "Question 1:\n";
+	cout << "Imagine a cup of water on the table. It is filled with water\n";
+	cout << "up to the half way point. Would you say the glass is...\n"
+	cout << "1. Half full\n";
+	cout << "2. Half empty\n";
+	cout << "3. 
+
+	return 0;
+}
