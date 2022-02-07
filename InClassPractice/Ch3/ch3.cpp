48eb636501c77ae90a8c86ab8ff646a0774e076a
BradenBellTTU <babell45@tntech.edu>
2022-02-07 16:49:22 -0600

    in class 3

---
+++ InClassPractice/Ch3/ch3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+int main() {
+	int choice;
+
+	cout << "Please choose from the following options:\n";
+	cout << "1. Fun with strings!\n"
+	cout << "2. Calculate area.\n"
+	cout << "CHOOSE 1 or 2: ";
+	cin >> choice;
+	switch (choice) {
+
+}
