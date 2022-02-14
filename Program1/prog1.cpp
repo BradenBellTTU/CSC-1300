35a0d543bd38c6bc63c61f7dc3a15a3378addfe0
BradenBellTTU <91089671+BradenBellTTU@users.noreply.github.com>
2022-02-14 16:52:03 -0600

    wtf is this

--- Program1/prog1.cpp
+++ Program1/prog1.cpp
@@ -4,53 +4,66 @@
 using namespace std;
 
 int main() {
-	bool doQuiz = true;
-	while(doQuiz == true) {
-		//Variables
-		int introvert = 0, extrovert = 0, positivity = 0, negativity = 0, silly = 0, serious = 0, spontaneous = 0, planner = 0;
-		char answer;
-		bool validAnswer = false;
+	int introvert = 0, extrovert = 0, positivity = 0, negativity = 0, silly = 0, serious = 0, spontaneous = 0, planner = 0;
+	string answer;
+	bool validAnswer = false;
+	while(true) {
+		
 
-
+		//Intro
 		cout << "\n\n-------------------------------" << endl;
 		cout << "      Personality Test         " << endl;
 		cout << "-------------------------------" << endl;
 		cout << "This is a 12 question personality test designed to gather data" << endl;
-		cout << "from 4 different aspects of your personality." << endl;
+		cout << "from 4 different aspects of your personality.\n\n\n\n" << endl;
 
+		//Question 1
 		cout << "Question 1:\n";
 		cout << "Imagine a cup of water on the table. It is filled with water\n";
 		cout << "up to the half way point. Would you say the glass is...\n";
-		cout << "1. Half full\n";
-		cout << "2. Half empty\n";
-		cout << "3. Both\n";
-
+		cout << "a. Half full\n";
+		cout << "b. Half empty\n";
+		cout << "c. Both\n";
 
 		while(validAnswer == false) {
-
-			cin >> answer;
+			getline(cin, answer);
+			answer.tolower();
 			switch(answer) {
-				case 1:
+				case 'a':
 					positivity++;
 					validAnswer = true;
 					break;
-				case 2:
+				case 'b':
 					negativity--;
 					validAnswer = true;
 					break;
-				case 3:
-					validAnswer = true;
+				case 'c':
 					positivity++;
 					negativity++;
+					validAnswer = true;
 					break;
 				default:
 					cout << "\nInvalid input. Please try again";
-					validAnswer = false;
 					break;
 			}
 		}
-		
+		cout << positivity << "\n" << negativity;
 
+		//TODO: DISPLAY RESULTS
+
+
+
+
+
+		cout << "Would you like to run the test again? y/n: ";
+		getline(cin, answer);
+		answer.tolower();
+		if (answer == "y"){
+			//Clear varibles
+			introvert = 0, extrovert = 0, positivity = 0, negativity = 0, silly = 0, serious = 0, spontaneous = 0, planner = 0;
+			validAnswer = false;
+		}
+		else 
+			return 0;
 	}
-	return 0;
 }
