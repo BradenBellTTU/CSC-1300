245b364b3071a8b7341c39513cf46c2ade23d3b6
BradenBellTTU <91089671+BradenBellTTU@users.noreply.github.com>
2022-02-13 21:04:18 -0600

    AHHHHHHHHH

--- Labs/Lab3/lab3a.cpp
+++ Labs/Lab3/lab3a.cpp
@@ -4,27 +4,34 @@
 using namespace std;
 
 int main() {
+
+    //Variables
     const float BASE_SHIRT_COST = 19.99, SALES_TAX = 0.0975;
     float billTotal, discountRate, finalShirtCost;
     int orderQuantity;
     string userName;
-    cout << "Welcome to the geek store, what is your name? ";
+
+    //Get name from user
+    cout << "\n\nWelcome to the geek store, what is your name? ";
     getline(cin, userName);
 
-    cout << "All shirts are $" << SHIRT_COST << ".\n We offer quantity discounts as follows: \n" << endl;
-    cout << setw(10) << "Number of Shirts" << setw(10) << "Discount\n";
-    cout << setw(10) << "5-10" << setw(10) << "10%\n";
-    cout << setw(10) << "11-20" << setw(10) << "15%\n";
-    cout << setw(10) << "21-30" << setw(10) << "20%\n";
-    cout << setw(10) << "31 or more" << setw(10) << "25%\n\n";
+    //Display discount rates
+    cout << "\nAll shirts are $" << BASE_SHIRT_COST << ".\nWe offer quantity discounts as follows: \n" << endl;
+    cout << setw(18) << "Number of Shirts" << setw(15) << "Discount\n";
+    cout << setw(6) << "5-10" << setw(22) << "10%\n";
+    cout << setw(7) << "11-20" << setw(21) << "15%\n";
+    cout << setw(7) << "21-30" << setw(21) << "20%\n";
+    cout << setw(12) << "31 or more" << setw(15) << "25%" << endl;
+    cout << "\n";
 
+    //Get number of shirts for order
     cout << userName << ", how many shirts would you like to order? ";
-    
     cin >> orderQuantity;
 
+    //Check discount brackets
     if (orderQuantity <= 0) {
         cout << "Invalid input: Please run the program again and enter a non-negative integer" << endl;
-        return 0
+        return 0;
     }
 
     else if ( (orderQuantity >= 5) && (orderQuantity <= 10) )
@@ -40,12 +47,18 @@ int main() {
         discountRate = 0.25;
 
 
+    //Calculate bill total
     finalShirtCost = BASE_SHIRT_COST - (discountRate * BASE_SHIRT_COST);
-    finalShirtCost = finalShirtCost - (finalShirtCost * SALES_TAX);
+    finalShirtCost = finalShirtCost + (finalShirtCost * SALES_TAX);
     billTotal = finalShirtCost * orderQuantity;
 
-    cout << "--------------------------------------------------\n";
-    cout << setw(10) << "BILL FOR:" << setw(10) << userName << "\n";
-    cout << setw(10) << "DISCOUNTED COST:" << setw(10) << "$" << finalShirtCost << " (per shirt)\n";
-    cout << setw(10) << "TOTAL COST:" << setw(10) << billTotal << endl;
+    //Display final bill to user
+    cout <<	setprecision(2) << fixed << showpoint;
+    cout << "--------------------------------------------------" << endl;
+    //I have no clue how to make setw() work and look good with all lengths of usernames
+    cout << "BILL FOR:" << setw(20) << userName << endl;
+    cout << "DISCOUNTED COST:" << setw(9) << "$" << finalShirtCost << " (per shirt)" << endl;
+    cout << "TOTAL COST:" << setw(14) << "$" << setprecision(2) << fixed << showpoint << billTotal << " (with tax)" << endl;
+
+    return 0;
 }
