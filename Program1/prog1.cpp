5af90916e52b20a53334aae48cc8aff72ecef270
BradenBellTTU <91089671+BradenBellTTU@users.noreply.github.com>
2022-02-14 18:14:22 -0600

    getten er done

--- Program1/prog1.cpp
+++ Program1/prog1.cpp
@@ -4,20 +4,22 @@
 using namespace std;
 
 int main() {
-	int introvert = 0, extrovert = 0, positivity = 0, negativity = 0, silly = 0, serious = 0, spontaneous = 0, planner = 0;
-	string answer;
-	bool validAnswer = false;
-	while(true) {
-		
+	bool doTest;
+	do {
+		//Variables
+		int introvert = 0, extrovert = 0, positivity = 0, negativity = 0, silly = 0, serious = 0, spontaneous = 0, planner = 0;
+	    char answer;
 
-		//Intro
+
+		//Display intro
 		cout << "\n\n-------------------------------" << endl;
 		cout << "      Personality Test         " << endl;
 		cout << "-------------------------------" << endl;
 		cout << "This is a 12 question personality test designed to gather data" << endl;
 		cout << "from 4 different aspects of your personality.\n\n\n\n" << endl;
 
-		//Question 1
+
+		//Question 1 dislay
 		cout << "Question 1:\n";
 		cout << "Imagine a cup of water on the table. It is filled with water\n";
 		cout << "up to the half way point. Would you say the glass is...\n";
@@ -25,45 +27,94 @@ int main() {
 		cout << "b. Half empty\n";
 		cout << "c. Both\n";
 
-		while(validAnswer == false) {
-			getline(cin, answer);
-			answer.tolower();
-			switch(answer) {
-				case 'a':
-					positivity++;
-					validAnswer = true;
-					break;
-				case 'b':
-					negativity--;
-					validAnswer = true;
-					break;
-				case 'c':
-					positivity++;
-					negativity++;
-					validAnswer = true;
-					break;
-				default:
-					cout << "\nInvalid input. Please try again";
-					break;
+		//Question 1 input
+		cin >> answer;
+		switch(answer) {
+			case 'a':
+				positivity++;
+				break;
+			case 'b':
+				negativity++;
+				break;
+			case 'c':
+				positivity++;
+				negativity++;
+				break;
+			default:
+				cout << "\nInvalid input. Please run the program again.\n";
+				return 0;
 			}
+	
+		//Question 2 display
+		cout << "\n\nQuestion 2:\n";
+		cout << "You wake up one morning and stub your toe on your dresser while getting ready\n";
+		cout << "You think to yourself...\n\n";
+		cout << "a. \"Ow! That hurt but I'm not going to let it ruin my day!\"\n";
+		cout << "b. \"Ow! Now my entire day is ruined, life sucks, I'm going back to bed!\"\n";
+		cout << "c. \"Ow!\"\n";
+
+		//Question 2 input
+		cin >> answer;
+		switch(answer) {
+			case 'a':
+				positivity++;
+				break;
+			case 'b':
+				negativity++;
+				break;
+			case 'c':
+				positivity++;
+				negativity++;
+				break;
+			default:
+				cout << "\nInvalid input. Please run the program again.\n";
+				return 0;
 		}
-		cout << positivity << "\n" << negativity;
+
+		//Question x display
+		cout << "\n\nQuestion x:\n";
+		cout << "\n";
+		cout << "\n\n";
+		cout << "a. \n";
+		cout << "b. \n";
+		cout << "c. \n";
+
+		//Question x input
+		cin >> answer;
+		switch(answer) {
+			case 'a':
+				//varible++;
+				break;
+			case 'b':
+				//variable++;
+				break;
+			case 'c':
+				//positivity++;
+				//negativity++;
+				break;
+			default:
+				cout << "\nInvalid input. Please run the program again.\n";
+				return 0;
+		}		
 
 		//TODO: DISPLAY RESULTS
 
-
+		cout << positivity << "\n" << negativity << "\n";
 
 
 
 		cout << "Would you like to run the test again? y/n: ";
-		getline(cin, answer);
-		answer.tolower();
-		if (answer == "y"){
+		cin >> answer;
+		if (answer == 'y'){
 			//Clear varibles
 			introvert = 0, extrovert = 0, positivity = 0, negativity = 0, silly = 0, serious = 0, spontaneous = 0, planner = 0;
-			validAnswer = false;
+			doTest = true;
 		}
-		else 
-			return 0;
+		else
+			doTest = false;
 	}
+
+
+	while(doTest == true);
+	return 0;
 }
