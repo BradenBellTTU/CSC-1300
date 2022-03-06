75556c5b39e7bd18fb6278abf08b993b84171045
BradenBellTTU <91089671+BradenBellTTU@users.noreply.github.com>
2022-03-06 15:49:05 -0600

    yuh

--- Labs/Lab6/lab6.cpp
+++ Labs/Lab6/lab6.cpp
@@ -3,16 +3,26 @@ using namespace std;
 
 int DisplayMenuGetChoice();
 void GoblinSickDays();
-int DisplayCoolMessage(int);
+int DisplayCoolMessage(int &);
 
 int main() {
     int coolMessageCount;
-
     switch(DisplayMenuGetChoice()){
 
         case 1:
             GoblinSickDays();
             break;
+        
+        case 2:
+            DisplayCoolMessage(coolMessageCount);
+            break;
+        
+        case 3:
+            return 0;
+        
+        default:
+            cout << "Unexpected Error. Exiting program.\n";
+            return 1;
     }
 }
 
@@ -34,6 +44,26 @@ int DisplayMenuGetChoice() {
         cin >> userChoice;
     }
 
+
     return userChoice;
+
+}
+
+void GoblinSickDays() {
+    int numGoblinTellers;
+    int NUM_YEARS = 3;
+    cout << "How many Goblin tellers worked at Gringotts during each of the last three years?\n";
+    cin >> numGoblinTellers;
+    while (cin.fail()) {
+        cin.clear();
+        cin.ignore(255, '\n');
+        cout << "Unexpected input. Please try again.\n;";
+        cout << "How many Goblin tellers worked at Gringotts during each of the last three years?\n";
+        cin >> numGoblinTellers;
+    }
+
+    for(int i = 0; i < NUM_YEARS; i++) {
+        for(numGoblinTellers; )
+    }
 
 }
