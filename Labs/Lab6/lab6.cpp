c498810faa403186022890272e26e4ea9f005cf5
BradenBellTTU <91089671+BradenBellTTU@users.noreply.github.com>
2022-03-07 16:27:47 -0600

    Polish

--- Labs/Lab6/lab6.cpp
+++ Labs/Lab6/lab6.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int DisplayCoolMessage(int&);
+//Prototype functions
+int DisplayCoolMessage(int);
 int DisplayMenuGetChoice();
 void GoblinSickDays();
 
-
+//Main
 int main() {
-    int coolMessageCount = 0;
+    
+    //Variables
+    int coolMessageCount;
     bool quit = false;
+
+    //Call appropriate function depending on what DisplayMenuGetFunction() returns (1-3) as long as quit = false
     do {
-        int coolMessageCount;
         switch(DisplayMenuGetChoice()){
 
             case 1:
@@ -18,7 +22,7 @@ int main() {
                 break;
             
             case 2:
-                DisplayCoolMessage(coolMessageCount);
+                coolMessageCount = DisplayCoolMessage(coolMessageCount);
                 break;
             
             case 3:
@@ -31,42 +35,63 @@ int main() {
         }
     }
     while(!quit);
+
+    //Close message
     cout << "You chose to display the cool message " << coolMessageCount << " time(s)!\n";
-    cout << "Bye!\n";
+    cout << "Bye!\n\n";
+    return 0;
 }
 
+
+//-==FUNCTION DEFINITIONS==-
+
+/*
++--------------------------------------------------------------------+
+| DipslayMenuGetChoice | Argument: none |                            |
+|--------------------------------------------------------------------|
+| Displays the main menu and returns an int 1-3 depending on the     |
+| user's choice of options in the main menu                          |
++--------------------------------------------------------------------+ */
 int DisplayMenuGetChoice() {
+    //Variables
     int userChoice;
 
+    //Display menu
     cout << "\n\nWELCOME TO GRINGOTTT's BANK!\n";
     cout << "Please choose from the following options:\n";
     cout << "1 - Enter Goblin Sick Days\n";
     cout << "2 - Display Something Cool\n";
     cout << "3 - End the Program\n";
-
     cout << "CHOICE: ";
+
+    //Get user choice
     cin >> userChoice;
-    while(cin.fail() || (userChoice <= 1 && userChoice >= 3)) {
+    cout << "\n\n";
+
+    //Input validation
+    while (cin.fail() || !(userChoice >= 1 && userChoice <= 3)) {
         cin.clear();
         cin.ignore(255, '\n');
         cout << "Invalid choice. Please choose 1-3.\n";
+        cout << "Please choose from the following options:\n";
+        cout << "1 - Enter Goblin Sick Days\n";
+        cout << "2 - Display Something Cool\n";
+        cout << "3 - End the Program\n";
+        cout << "CHOICE: ";
         cin >> userChoice;
     }
-
-
     return userChoice;
-
 }
+
 
 /*
 +--------------------------------------------------------------------+
-| GoblinSickDays |  No input. | void |                               |
+| GoblinSickDays |  No arguments |                                   |
 |--------------------------------------------------------------------|                          
 | Asks the user how many Goblin tellers there are and their          |
 | sick days. It then displays the total number of sick days taken    |
 | by all Goblin tellers over the past threee years.                  |
-+--------------------------------------------------------------------+
-*/
++--------------------------------------------------------------------+ */
 void GoblinSickDays() {
     int numGoblinTellers, userSickDays = 0, totalSickDays = 0;
     const int NUM_YEARS = 3;
@@ -80,6 +105,7 @@ void GoblinSickDays() {
         cin >> numGoblinTellers;
     }
 
+    //Nested for loop to calculate sick days
     for (int i = 1; i <= numGoblinTellers; i++) {
         for (int j = 1; j <= NUM_YEARS; j++) {
             cout << "How many sick days was Goblin " << i << " out for, duing year " << j << "? ";
@@ -91,17 +117,25 @@ void GoblinSickDays() {
                 cout << "How many sick days was Goblin " << i << " out for, duing year " << j << "? ";
                 cin >> userSickDays;
             }
-
             totalSickDays += userSickDays;
-            cout << totalSickDays << endl;
         }
     }
 
+    //Display total sick day message
     cout << "The " << numGoblinTellers << " Goblins were out sick for a total of " << totalSickDays << " days during the last 3 years";
 
 }
 
-int DisplayCoolMessage(int &coolMessageCount) {
+
+/*
++--------------------------------------------------------------------+
+| DisplayCoolMessage | Argument: coolMessageCount |                  |
+|--------------------------------------------------------------------|
+| Displays a cool message and keeps track of how many times it has   |
+| been displayed                                                     |
++--------------------------------------------------------------------+ */
+int DisplayCoolMessage(int coolMessageCount) {
+    cout << "\n\n";
     cout << " /$$$$$$$$ /$$   /$$ /$$$$$$$$        /$$$$$$   /$$$$$$  /$$   /$$ /$$$$$$$$       /$$$$$$  /$$$$$$         /$$$$$$        /$$       /$$$$$$ /$$$$$$$$\n";
     cout << "|__  $$__/| $$  | $$| $$_____/       /$$__  $$ /$$__  $$| $$  /$$/| $$_____/      |_  $$_/ /$$__  $$       /$$__  $$      | $$      |_  $$_/| $$_____/\n";
     cout << "   | $$   | $$  | $$| $$            | $$  \\__/| $$  \\ $$| $$ /$$/ | $$              | $$  | $$  \\__/      | $$  \\ $$      | $$        | $$  | $$      \n";
