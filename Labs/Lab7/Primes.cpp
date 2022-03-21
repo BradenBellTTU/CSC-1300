288ea1c6212be69cd2e147d3ae4e5c334f6658a7
BradenBellTTU <91089671+BradenBellTTU@users.noreply.github.com>
2022-03-21 15:55:37 -0500

    ihatemylife

--- Labs/Lab7/Primes.cpp
+++ Labs/Lab7/Primes.cpp
@@ -1,9 +1,27 @@
 #include "Primes.h"
+#include <iostream>
+#include <string>
+using namespace std;
 
 void findPrimes(int lowerRange, int upperRange, string fileName) {
+    for (int i = lowerRange; i < upperRange; i++) {
+        if(isPrime(i)) {
+            //output i to file
+        }
+    }
+
 
 }
 
-int isPrime(int) {
-    return 0;
+bool isPrime(int x) {
+    int factorCount = 0;
+
+    for (int i = 1; i < x; i++) {
+        if ((x % i) == 0) 
+            factorCount++;
+
+        if factorCount > 2
+            return false;         
+    }
+    return true;
 }
