71b416c21188c6fa51cb86e93d6a63c4ec01df9b
BradenBellTTU <91089671+BradenBellTTU@users.noreply.github.com>
2022-03-28 14:33:16 -0500

    hhhhhhhhh

--- Labs/Lab8/Driver.cpp
+++ Labs/Lab8/Driver.cpp
@@ -14,6 +14,7 @@
 using namespace std;
 
 int main() {
+    //Define vars
     const int SIZE = 10;
     float foodTotal = 0, foodAverage = 0;
     int mostFoodEaten = 0;
@@ -21,21 +22,26 @@ int main() {
     float poundsEaten[SIZE];
     string elephantNames[SIZE];
 
+    //Get name of zoo and store it in a cstring
     cout << "Hello! What is the name of your zoo?\n";
     cin.getline(zooName, 100);
 
+    //Call functions
     getElephantData(poundsEaten, elephantNames, SIZE);
 
     getStats(poundsEaten, SIZE, foodTotal, foodAverage, mostFoodEaten);
 
+    //Print line of dashes
     for (int i = 0; i < 80; i++)
         cout << "-";
 
+    //Display results
     cout << setprecision(2) << fixed << showpoint;
     cout << "\nThe total amount of food consumed by all " << SIZE << " elephants at " << zooName << " is " << foodTotal << " in one month.\n";
     cout << "The average amount of food consimed at " << zooName << " is " << foodAverage << " pounds.\n\n\n";
     cout << "The elephant who eats the most (" << poundsEaten[mostFoodEaten] << " pounds) is " << elephantNames[mostFoodEaten] << "!\n";
 
+    //Print line of dashes
     for (int i = 0; i < 80; i++)
         cout << "-";
 }
