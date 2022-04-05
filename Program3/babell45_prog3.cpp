aff17e7ecd9284025cd0641b7967c767069946dc
BradenBellTTU <91089671+BradenBellTTU@users.noreply.github.com>
2022-04-05 15:10:25 -0500

    yeeeeeeeee

--- Program3/babell45_prog3.cpp
+++ Program3/babell45_prog3.cpp
@@ -14,12 +14,14 @@
 #include <iomanip>
 using namespace std;
 
+
 int carnOrHerb(string);
 bool searchCarnivore(string);
 bool searchHerbivore(string);
 void printDino(ofstream&, ifstream&, string, int[]);
 bool overTenGrand(string);
 void printResults(int[], string[]);
+
 
 int main() {
     ifstream inFileStream;
@@ -45,12 +47,68 @@ int main() {
         inFileStream.open(dinoDirName);
     }
 
-    //Read data from directory file
+    cout << searchCarnivore("Tyrannosaurus Rex") << endl;
+    cout << searchCarnivore("hello") << endl;
+    cout << searchHerbivore("Triceratops") << endl;
+    cout << searchHerbivore("Hello") << endl;
+    /*Read data from directory file
     while(getline(inFileStream, tempString,'#')) {
         cout << tempString << ' ';
     }
+    */
 
+   inFileStream.close();
     return 0;
+}
+
+bool searchCarnivore(string animalName) {
+    //Function vars
+    ifstream inFileStream;
+    string tempString;
+    inFileStream.open("carnivores.txt");
+
+    //Check to see if the file is open, if not return false automatically
+    if (!(inFileStream.is_open())) {
+        cout << "Error opening carnivores.txt\n";
+        return false;
+    }
+
+    //Iterate through carnivore.txt and compare everything to animalName
+    while(getline(inFileStream, tempString, '#')) {
+        if (tempString == animalName) {
+            inFileStream.close();
+            return true;
+        }
+    }
+
+    //Catch all
+    inFileStream.close();
+    return false;
+}
 
 
+
+bool searchHerbivore(string animalName) {
+    //Function vars
+    ifstream inFileStream;
+    string tempString;
+    inFileStream.open("herbivores.txt");
+
+    //Check to see if the file is open, if not return false automatically
+    if (!(inFileStream.is_open())) {
+        cout << "Error opening herbivore.txt\n";
+        return false;
+    }
+
+    //Iterate through carnivore.txt and compare everything to animalName
+    while(getline(inFileStream, tempString, '#')) {
+        if (tempString == animalName) {
+            inFileStream.close();
+            return true;
+        }
+    }
+
+    //Catch all
+    inFileStream.close();
+    return false;
 }
