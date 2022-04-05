1ef9486a6cfad96ae34721bf8394a73dabec5b7b
BradenBellTTU <91089671+BradenBellTTU@users.noreply.github.com>
2022-04-05 10:36:17 -0500

    Lab10 Examples

--- Labs/Lab10/Examples/Functions.cpp
+++ Labs/Lab10/Examples/Functions.cpp
@@ -7,4 +7,5 @@ int* makeArray(int numDays) {
 void arrEdit(int* arr) {
     for (int i = 0; i < num; x++) 
         cout << *(arr + i); //This is like arr[i] but for heap arrays.
+        total += (*(arr+i));  //Assigining total to arr[i]
 }
--- Labs/Lab10/Examples/driver.cpp
+++ Labs/Lab10/Examples/driver.cpp
@@ -8,4 +8,14 @@ int main() {
 
     //myArr[0] is not very efficient with pointer or heap arrays
     //So we'll have to do memory arithmatic
+
+    //Static casting
+    float var2 = static_cast<float>(days);
+
+    //You have to free your memory!!!!
+    //This is to prevent memory leaks
+    delete [] myArr;
+
+    //End program normally
+    return 0;
 }
